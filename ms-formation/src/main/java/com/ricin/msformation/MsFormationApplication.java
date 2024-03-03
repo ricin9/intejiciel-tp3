519391a924061325e1d7293de9fa4793393508fd
ricin9 <ricin9@outlook.com>
2024-03-03 09:49:43 +0100

    ha cha baghi dir

---
+++ ms-formation/src/main/java/com/ricin/msformation/MsFormationApplication.java
@@ -0,0 +1,13 @@
+package com.ricin.msformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsFormationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsFormationApplication.class, args);
+    }
+
+}
